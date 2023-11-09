c7c138ff534b05fc9af7cc7c97b479fd6ea3f2ac
Trincademes <133989401+Trincademes@users.noreply.github.com>
2023-11-09 08:20:09 -0300

    Delete PrjLivro directory

--- PrjLivro/src/main/java/com/senai/PrjLivro/Repository/LivroRepository.java
+++
@@ -1,9 +0,0 @@
-package com.senai.PrjLivro.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.senai.PrjLivro.Entities.Livro;
-
-public interface LivroRepository extends JpaRepository<Livro, Long>{
-	
-}
